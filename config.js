c47f7f85fcc7f5db7aa53e9ede707a08ffd9962e
azertyiuop <leyiseverywhere@gmail.com>
2025-12-28 01:26:37 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const GITHUB_CONFIG = {
-    token: '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', // TOKEN GIT
-    gistId: null
-};
-
-// Exposer les valeurs sur window pour que common.js puisse les utiliser
-window.GITHUB_TOKEN = GITHUB_CONFIG.token;
-window.GITHUB_GIST_ID = GITHUB_CONFIG.gistId;
